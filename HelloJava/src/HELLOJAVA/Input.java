849ece6cdaf1b6d0b98c1c108472a96834005d07
TeppuSan <suraimu0206@icloud.com>
2025-06-30 13:35:41 +0900

    negative text

--- HelloJava/src/HELLOJAVA/Input.java
+++ HelloJava/src/HELLOJAVA/Input.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("What is your goal?");
-        String goal = scanner.nextLine();
-        if (goal == null || goal.isEmpty()) {
-            System.out.println(goal + "Oh, nothing at all? How... impressive.");
-            scanner.close();
+        Scanner scanner = new Scanner(System.in);// scannerを開いて後で入力措置をとれるようにしている
+        System.out.print("What is your goal?");// 目標を聞く
+        String goal = scanner.nextLine();// String型に値を入力する
+        if (goal == null || goal.isEmpty()) {// null又は[]入力の際の処理
+            System.out.println(goal + "Oh, nothing at all? How... impressive.");// わざとgoal+を入れることでより厭味ったらしく
+            scanner.close();// 開いたscannerを閉じる
             return;
         }
-        System.out.println(goal + "? Oh, how very ambitious of you.");
-        System.out.println("Truly inspiring... if only it were realistic.");
-        scanner.close();
+        System.out.println(goal + "? Oh, how very ambitious of you.");// 厭味ったらしく
+        System.out.println("Truly inspiring... if only it were realistic.");// 厭味ったらしく
+        scanner.close();// 開いたscannerを閉じる
     }
 }
